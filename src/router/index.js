c6e5a86bf202ed8b5b3bbbfec5380232a1622f9d
Ilya <ilha.eliseenko@yandex.by>
2024-03-18 11:03:19 +0300

    Add login component

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import Login from "@/views/Login.vue";
 
 const routes = [
   {
-    path: "/",
+    path: "/home",
     name: "home",
     component: HomeView,
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: Login,
   },
 ];
 
